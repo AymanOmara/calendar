118ccbb14d3176984c16d68ef0b960dcc10ab2c3
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2023-11-30 22:06:52 +0200

    commit

--- lib/calender_cubit.dart
+++ lib/calender_cubit.dart
@@ -1,73 +1,58 @@
-import 'dart:developer';
-
 import 'package:calender/calender_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:collection/collection.dart';
 class CalenderCubit extends Cubit<CalenderStats> {
   CalenderCubit() : super(CalenderInitial()) {
-    var v =  daysInMonth();
-    for (int i = 1; i<= daysInMonth();i++){
-      var date = DateTime(dateTime.year,dateTime.month,i);
-      times.add(MyDate(date: date, dayName: date.day.toString(), dayNumber: "$i"));
+    var yearMonths = List<int>.generate(12, (i) => i + 1);
+    for(int month in yearMonths){
+      times.addAll(_generateItemsFromMonth(month: month));
     }
-    // times.insertAll(0,_generateItemsForDate(dateTime: DateTime(dateTime.year,dateTime.month-1,0)));
     emit(CalenderInitial());
   }
   DateTime dateTime = DateTime.now();
   List<MyDate> times = [];
 
   void nextPage() {
-    dateTime = DateTime(dateTime.year,dateTime.month+1,1);
-    print(times.firstWhereOrNull((element) => element.date.compareTo(dateTime)==0));
-    if(times.firstWhereOrNull((element) => element.date.compareTo(dateTime)==0) != null){
-      return;
-    }
-    // return;
-    for (int i = 1; i<= daysInMonth();i++){
-      var date = DateTime(dateTime.year,dateTime.month,i);
-      times.add(MyDate(date: date, dayName: date.day.toString(), dayNumber: "$i"));
-    }
-    emit(CalenderInitial());
+    //  dateTime = DateTime(dateTime.year,dateTime.month+1,1);
+    // if(times.firstWhereOrNull((element) => element.date.compareTo(dateTime)==0) != null){
+    //   emit(CalenderInitial());
+    //   return;
+    // }
+    // // dateTime = tempDate;
+    // if(times.firstWhereOrNull((element) => element.date.compareTo(dateTime)==0) != null){
+    //   return;
+    // }
+    // for (int i = 1; i<= daysInMonth();i++){
+    //   var date = DateTime(dateTime.year,dateTime.month,i);
+    //   times.add(MyDate(date: date, dayName: date.day.toString(), dayNumber: "$i"));
+    // }
+    // emit(CalenderInitial());
   }
 
   void previousPage() {
-  //   dateTime = DateTime(dateTime.year,dateTime.month-1,1);
-  //   print(times.firstWhereOrNull((element) => element.date.compareTo(dateTime)==0));
-  //
-  //   if(times.firstWhereOrNull((element) => element.date.compareTo(dateTime)==0) != null){
-  //     return;
-  //   }
-  //   // return;
-  //   for (int i = daysInMonth(); i>= 1;i--){
-  //     var date = DateTime(dateTime.year,dateTime.month,i);
-  //     times.insert(0, MyDate(date: date, dayName: date.day.toString(), dayNumber: "$i"));
-  //   }
-  //   log(times.map((e) => e.date).toList().toString());
-  //   emit(CalenderInitial());
+    // var  tempDate = DateTime(dateTime.year,dateTime.month-1,1);
+    // if(times.firstWhereOrNull((element) => element.date.compareTo(tempDate)==0) != null){
+    //   return;
+    // }
+    // dateTime = tempDate;
+    // for (int i = daysInMonth(); i>= 1;i--){
+    //   var date = DateTime(dateTime.year,dateTime.month,i);
+    //   times.insert(0, MyDate(date: date, dayName: date.day.toString(), dayNumber: "$i"));
+    // }
+    // emit(CalenderInitial());
   }
 
 
-  int daysInMonth() {
-    var firstDayThisMonth = dateTime;
+  int daysInMonth(int month) {
+    var firstDayThisMonth = DateTime(DateTime.now().year,month,1);
     var firstDayNextMonth = DateTime(firstDayThisMonth.year,
         firstDayThisMonth.month + 1, firstDayThisMonth.day);
     return firstDayNextMonth.difference(firstDayThisMonth).inDays;
   }
-  List<MyDate> _generateItemsForNextMonth({required DateTime dateTime}){
+  List<MyDate> _generateItemsFromMonth({required int month}){
     List<MyDate> times = [];
-    dateTime = DateTime(dateTime.year,dateTime.month,1);
-    for (int i = daysInMonth(); i>= 1;i--){
-      var date = DateTime(dateTime.year,dateTime.month,i);
-      times.insert(0, MyDate(date: date, dayName: date.day.toString(), dayNumber: "$i"));
-    }
-    return times;
-  }
-  List<MyDate> _generateItemsForPreviousMonth({required DateTime dateTime}){
-    List<MyDate> times = [];
-    dateTime = DateTime(dateTime.year,dateTime.month,1);
-    for (int i = daysInMonth(); i>= 1;i--){
-      var date = DateTime(dateTime.year,dateTime.month,i);
-      times.insert(0, MyDate(date: date, dayName: date.day.toString(), dayNumber: "$i"));
+    for (int i = 1; i<= daysInMonth(month);i++){
+      var date = DateTime(dateTime.year,month,i);
+      times.add(MyDate(date: date, dayName: date.day.toString(), dayNumber: "$i"));
     }
     return times;
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:calender/calender_cubit.dart';
 import 'package:calender/calender_states.dart';
+import 'package:easy_date_timeline/easy_date_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -16,21 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -53,17 +39,15 @@ class Calender extends StatelessWidget {
           CalenderCubit cubit = BlocProvider.of(context);
           scroll.addListener(() {
             var nextPageTrigger = 0.93 * scroll.position.maxScrollExtent;
-            var previousPageTrigger = scroll.position.extentBefore;
             if (scroll.position.pixels > nextPageTrigger) {
+              print("next page");
+
               cubit.nextPage();
             }
-            if (scroll.position.pixels == 0) {
-              cubit.previousPage();
-              print("item fixed");
+            if (scroll.position.pixels <= 100) {
+              print("previous page");
+              //cubit.previousPage();
             }
-            // if(previousPageTrigger <= 300){
-            //
-            // }
             if (state is CalenderInitial) {}
           });
           return Scaffold(
